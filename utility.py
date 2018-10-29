0a355af00fe248577f1e3ffb69274771ef6cd1e2
Reynok <reynokang@gmail.com>
2018-10-29 09:56:57 +0800

    修复lang不在的bug

--- utility.py
+++ utility.py
@@ -39,7 +39,10 @@ class HighlightRenderer(mistune.Renderer):
         if not lang:
             return '\n<pre><code>%s</code></pre>\n' % \
                 mistune.escape(code)
-        lexer = get_lexer_by_name(lang, stripall=True)
+        try:
+            lexer = get_lexer_by_name(lang, stripall=True)
+        except ClassNotFound as e:
+            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
         formatter = html.HtmlFormatter()
         return highlight(code, lexer, formatter)
 
